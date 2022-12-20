5e9e3292fbc307ef03817235e6b19879602f3331
Gustavo-Blois <104032155+Gustavo-Blois@users.noreply.github.com>
2022-12-20 15:28:43 -0300

    Update avl.c

--- avl.c
+++ avl.c
@@ -24,7 +24,94 @@ int max(int a, int b)
 {
     return (a > b)? a : b;
 }
+
+struct Node * minValueNode(struct Node* node)
+{
+    struct Node* current = node;
  
+    while (current->left != NULL)
+    {
+          current = current->left;
+    }
+    return current;
+}
+struct Node* deleteNode(struct Node* root, int key)
+{
+
+    if (root == NULL)
+    {
+        return root;
+    }
+
+ 
+    if ( key < root->key )
+    {
+        root->left = deleteNode(root->left, key);
+    }
+ 
+    else if( key > root->key )
+    {
+        root->right = deleteNode(root->right, key);
+    }
+
+    else
+    {
+        if( (root->left == NULL) || (root->right == NULL) )
+        {
+            struct Node *temp = root->left ? root->left :
+                                             root->right;
+  
+            // No child case
+            if (temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else
+            {
+             *root = *temp;
+            }
+            free(temp);
+        }
+        else
+        {
+            struct Node* temp = minValueNode(root->right);
+            root->key = temp->key;
+            root->right = deleteNode(root->right, temp->key);
+        }
+    }
+    if (root == NULL)
+    {
+      return root;
+    }
+    root->height = 1 + max(height(root->left),height(root->right));
+ 
+    int balance = getBalance(root);
+ 
+    if (balance > 1 && getBalance(root->left) >= 0){
+        return rightRotate(root);
+    }
+ 
+    if (balance > 1 && getBalance(root->left) < 0)
+    {
+        root->left =  leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    if (balance < -1 && getBalance(root->right) <= 0)
+    {
+        return leftRotate(root);
+    }
+ 
+    if (balance < -1 && getBalance(root->right) > 0)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+  
+    return root;
+} 
+
 struct Node* newNode(int key)
 {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
